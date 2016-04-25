375282c17cc8b4b28a8015b2c0e6af9409de4cff
LCY <GuoBin@mac-mini.local>
2016-04-25 15:55:41 +0800

    Initial Commit

---
+++ CYPanSwipeGesture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CYPanSwipeGesture
+//
+//  Created by 薛国宾 on 16/4/25.
+//  Copyright © 2016年 千里之行始于足下. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CYPanSwipeGestureTests/CYPanSwipeGestureTests.m
@@ -0,0 +1,39 @@
+//
+//  CYPanSwipeGestureTests.m
+//  CYPanSwipeGestureTests
+//
+//  Created by 薛国宾 on 16/4/25.
+//  Copyright © 2016年 千里之行始于足下. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CYPanSwipeGestureTests : XCTestCase
+
+@end
+
+@implementation CYPanSwipeGestureTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
